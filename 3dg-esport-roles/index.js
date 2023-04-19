bc716dece15254904fc87c6c9a2707d02b676721
marcel-noehre <marcelnoehre@web.de>
2023-04-19 15:59:35 +0200

    add prototype button with dropdown menu

--- 3dg-esport-roles/index.js
+++ 3dg-esport-roles/index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
 const auth = require('./assets/auth.json');
 
 const client = new Client({
@@ -18,13 +18,42 @@ client.on('messageCreate', async msg => {
     if (msg.content === '!3dg-submit') {
         const row = new ActionRowBuilder()
             .addComponents(
+                new StringSelectMenuBuilder()
+                .setCustomId('starter')
+                .setPlaceholder('Make a selection!')
+                .addOptions(
+                    new StringSelectMenuOptionBuilder()
+                    .setLabel('Bulbasaur')
+                    .setDescription('The dual-type Grass/Poison Seed Pokémon.')
+                    .setValue('bulbasaur'),
+                    new StringSelectMenuOptionBuilder()
+                    .setLabel('Charmander')
+                    .setDescription('The Fire-type Lizard Pokémon.')
+                    .setValue('charmander'),
+                    new StringSelectMenuOptionBuilder()
+                    .setLabel('Squirtle')
+                    .setDescription('The Water-type Tiny Turtle Pokémon.')
+                    .setValue('squirtle'),
+                )
+            );
+        const row2 = new ActionRowBuilder()
+            .addComponents(
                 new ButtonBuilder()
                 .setCustomId('submit_button')
                 .setLabel('Abschicken')
                 .setStyle('Secondary')
             );
-        msg.channel.send({ components: [row] })
-            .then(console.log('Submit-Button generated!'));
+
+        msg.channel.send({ components: [row, row2] })
+            .then(console.log('Submit-Button generated!'))
+    }
+});
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isButton()) {
+        return;
+    } else {
+
     }
 });
 
